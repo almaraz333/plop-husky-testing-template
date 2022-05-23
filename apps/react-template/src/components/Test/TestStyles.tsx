7221724d89c6a97ec7b35188fff598d2be27ae3c
Colton Almaraz <almarazcolton333@gmail.com>
2022-05-23 13:40:39 -0700

    Refactor: moved cypress files

--- apps/react-template/src/components/Test/TestStyles.tsx
+++ apps/react-template/src/components/Test/TestStyles.tsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
 import { TestProps } from './types';
 
-export const StyledTest = styled.div<TestProps>``;
+export const StyledTest = styled.div<TestProps>`
+margin: 0;
+margin:0;
+`;
