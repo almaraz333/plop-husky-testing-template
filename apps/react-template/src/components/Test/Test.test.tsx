2b1b12c7c68132c628f53b4a1ad497f629848f36
Colton Almaraz <almarazcolton333@gmail.com>
2022-05-23 13:54:09 -0700

    Fix: removed intentional error to test husky pre push

--- apps/react-template/src/components/Test/Test.test.tsx
+++
@@ -1,9 +0,0 @@
-import { mount } from "@cypress/react";
-import { Test } from ".";
-
-describe('Test test', () => {
-  it('successfully gets content', () => {
-    mount( <Test />);
-    cy.contains('Test');
-  });
-});
--- apps/react-template/src/components/Test/TestStyles.tsx
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-import { TestProps } from './types';
-
-export const StyledTest = styled.div<TestProps>`
-margin: 0;
-margin:0;
-`;
